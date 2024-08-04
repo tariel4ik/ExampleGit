dcd6becec4d1a36ddbade0cb44d7a778e461c683
tariel4ik <tarieltariel64@gmail.com>
2024-08-04 14:01:56 +0300

    ssss

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        int n = 11;
+        System.out.println("Тарик крутой " + n);
     }
 }
