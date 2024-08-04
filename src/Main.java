5dee7bf3f6be2a44817cb4c333c59e7c1124fcb6
tariel4ik <tarieltariel64@gmail.com>
2024-08-04 14:16:34 +0300

    demo 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 11;
-        System.out.println("Тарик крутой " + n);
         System.out.println("Я крут");
     }
 }
